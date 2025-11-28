241df3ea90f2ab31357a79eeda6539a16580e66d
FerdisJ <161576868+FerdisJ@users.noreply.github.com>
2025-11-28 11:57:06 +0100

    Useless imports removed

--- src/components/ui/PolicyHeader.tsx
+++ src/components/ui/PolicyHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import GLOBAL_STYLES from '../../styles/styles';
-import { COMPONENTS, COLORS } from '../../styles/theme';
+import { COMPONENTS } from '../../styles/theme';
 
 interface PolicyHeaderProps {
   title: string;
